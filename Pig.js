fb408ecf07836525c47b06ee1f30d26522e1e5f7
coderanishka <62501236+coderanishka@users.noreply.github.com>
2020-04-07 16:50:43 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,32 @@
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/enemy.png");
+    this.visibility = 255;
+  }
+
+  score(){
+   if(this.visibility < 0 && this.visibility > - 1000){
+     score++
+   }
+
+  }
+
+display(){
+
+if(this.body.speed<3.5){
+  super.display()
+}else{
+  
+  World.remove(world,this.body);
+ 
+  push();
+  this.visibility-=5;
+  tint(255,this.visibility);
+  image(this.image,this.body.position.x,this.body.position.y,50,50);
+  pop();
+}
+
+}
+
+};
